3e85ddadf41dac2fefd300d1c00b115c686d34c1
krish2956-mk <marakana009@gmail.com>
2025-05-13 19:55:41 +0530

    improved navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,47 +3,48 @@ import Link from "next/link";
 import { Bodoni_Moda } from "next/font/google";
 
 const bodoni = Bodoni_Moda({
-  weight: ["600"],
+  weight: "600",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const Navbar = () => {
   return (
-    <nav className="navbar flex justify-between items-center p-2 px-5 py-6 mb-5 border-b-2 border-gray-400 border-opacity-50">
-      <h1 className={`${bodoni.className} text-left px-6 tracking-widest text-xl text-black`}>
+    <nav className="flex  items-center px-8 py-5 border-b border-gray-300">
+      <h1 className={`${bodoni.className} tracking-widest text-xl text-black`}>
         GETNOMIK
       </h1>
-      <ul className="flex space-x-[70px] text-lg px-4 md:px-10 py-2 text-black">
+      <ul className="flex space-x-16 text-lg px-[35px]  text-black">
         <li>
-          <Link
-            href="/"
-            className="hover:text-gray-600 transition-transform transform hover:translate-y-[-3px]"
-          >
-            home
+          <Link href="/" className="group relative inline-block">
+            <span className="hover:text-green-700 transition-colors duration-300">
+              home
+            </span>
+            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-black group-hover:w-full transition-all duration-300"></span>
           </Link>
         </li>
         <li>
-          <Link
-            href="/items"
-            className="hover:text-gray-600 transition-transform transform hover:translate-y-[-3px]"
-          >
-            items
+          <Link href="/items" className="group relative inline-block">
+            <span className="hover:text-green-700 transition-colors duration-300">
+              items
+            </span>
+            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-black group-hover:w-full transition-all duration-300"></span>
           </Link>
         </li>
         <li>
-          <Link
-            href="/about"
-            className="hover:text-gray-600 transition-transform transform hover:translate-y-[-3px]"
-          >
-            about
+          <Link href="/about" className="group relative inline-block">
+            <span className="hover:text-green-700 transition-colors duration-300">
+              about
+            </span>
+            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-black group-hover:w-full transition-all duration-300"></span>
           </Link>
         </li>
         <li>
-          <Link
-            href="/contact"
-            className="hover:text-gray-600 transition-transform transform hover:translate-y-[-3px]"
-          >
-            contact
+          <Link href="/contact" className="group relative inline-block">
+            <span className="hover:text-green-700 transition-colors duration-300">
+              contact
+            </span>
+            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-black group-hover:w-full transition-all duration-300"></span>
           </Link>
         </li>
       </ul>
